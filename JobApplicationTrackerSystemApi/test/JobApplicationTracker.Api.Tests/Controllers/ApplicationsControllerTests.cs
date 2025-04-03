831c8733a4fc4a555a213767935d311149b124ea
CameronMcDougall <>
2025-04-03 18:37:24 +1300

    add remaining integration tests

--- JobApplicationTrackerSystemApi/test/JobApplicationTracker.Api.Tests/Controllers/ApplicationsControllerTests.cs
+++ JobApplicationTrackerSystemApi/test/JobApplicationTracker.Api.Tests/Controllers/ApplicationsControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
+using JobApplicationTracker.Api.Models.Requests;
 using JobApplicationTracker.Api.Models.Responses;
 using JobApplicationTracker.Api.Models.Shared;
 using JobApplicationTracker.Domain;
@@ -15,33 +16,38 @@ public class ApplicationsControllerTests
     private const string ApplicationsBaseUrl = "applications";
     private readonly HttpClient _client;
     private readonly JobApplicationTrackerDbContext _dbContext;
+
     public ApplicationsControllerTests()
     {
         CustomWebApplicationFactory<TestStartup> factory = new();
-        _client = factory.CreateClient(new WebApplicationFactoryClientOptions
-        {
-            AllowAutoRedirect = false
-        });
+        _client = factory.CreateClient(
+            new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            }
+        );
         _dbContext = factory.Services.GetRequiredService<JobApplicationTrackerDbContext>();
     }
 
     [Fact]
-    public async Task GetAsync_ValidApplication_ReturnsOK()
+    public async Task GetApplication_ValidApplication_ReturnsOK()
     {
-        var application = new Application("Datacom",
+        var application = new Application(
+            "Datacom",
             "Developer",
             ApplicationStatus.Interview,
-            new DateTime(2025, 04, 02));
+            new DateTime(2025, 04, 01)
+        );
         _dbContext.Applications.Add(application);
         await _dbContext.SaveChangesAsync();
 
         var response = await _client.GetAsync($"{ApplicationsBaseUrl}/{application.Id}");
-        
+
         Assert.NotNull(response);
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
         var responseBody = await response.Content.ReadFromJsonAsync<GetApplicationResponseDto>();
-        
+
         Assert.NotNull(responseBody);
         Assert.NotNull(responseBody.Application);
 
@@ -52,17 +58,86 @@ public class ApplicationsControllerTests
     }
 
     [Fact]
-    public async Task GetAsync_NotExistantApplication_Returns404()
+    public async Task GetApplication_NotExistantApplication_Returns404()
     {
-        var application = new Application("Datacom",
+        var application = new Application(
+            "Datacom",
             "Developer",
             ApplicationStatus.Interview,
-            new DateTime(2025, 04, 02));
+            new DateTime(2025, 04, 02)
+        );
         _dbContext.Applications.Add(application);
         await _dbContext.SaveChangesAsync();
 
         var result = await _client.GetAsync($"{ApplicationsBaseUrl}/{application.Id}");
         Assert.NotNull(result);
-        Assert.Equal(HttpStatusCode.NotFound ,result.StatusCode);
+        Assert.Equal(HttpStatusCode.NotFound, result.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetApplications_WithPagination_Returns404()
+    {
+
+        for (var i = 0; i < 10; i++)
+        {
+            var application = new Application(
+                "Datacom",
+                "Developer",
+                ApplicationStatus.Interview,
+                new DateTime(2025, 04, 02)
+            );
+            _dbContext.Applications.Add(application);
+        }
+
+        await _dbContext.SaveChangesAsync();
+
+        var result = await _client.GetAsync($"{ApplicationsBaseUrl}?pageSize=20&pageNumber=0");
+        Assert.NotNull(result);
+        Assert.Equal(HttpStatusCode.NotFound, result.StatusCode);
+    }
+
+    [Fact]
+    public async Task PostAsync_NewApplication_Returns201()
+    {
+        var request = new AddApplicationRequestDto
+        {
+            Position = "Developer",
+            CompanyName = "Datacom",
+            Status = ApplicationStatusDto.Offer,
+            DateApplied = new DateTime(2025, 04, 03)
+        };
+
+        var result = await _client.PostAsJsonAsync($"{ApplicationsBaseUrl}", request);
+        Assert.NotNull(result);
+        Assert.Equal(HttpStatusCode.Created, result.StatusCode);
+
+        var application = _dbContext.Applications.FirstOrDefault(e => e.AppliedDate == request.DateApplied);
+        Assert.NotNull(application);
+        Assert.Equal(request.DateApplied, application.AppliedDate);
+        Assert.Equal(request.Position, application.Position);
+        Assert.Equal(request.CompanyName, application.CompanyName);
+        Assert.Equal(ApplicationStatus.Offer, application.Status);
+    }
+
+    [Fact]
+    public async Task PatchAsync_NewApplication_Returns204()
+    {
+        var application = new Application(
+            "Datacom",
+            "Developer",
+            ApplicationStatus.Interview,
+            new DateTime(2025, 04, 04)
+        );
+        _dbContext.Applications.Add(application);
+        await _dbContext.SaveChangesAsync();
+
+        var request = new UpdateApplicationRequestDto
+        {
+            Status = ApplicationStatusDto.Offer
+        };
+
+        var result = await _client.PatchAsJsonAsync($"{ApplicationsBaseUrl}/{application.Id}", request);
+        Assert.NotNull(result);
+        Assert.Equal(HttpStatusCode.NoContent, result.StatusCode);
     }
 }
